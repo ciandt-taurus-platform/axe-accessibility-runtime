a1341495546d14fb4305ec81143e3ea3589dd0b9
Leonardo Zamariola <leonardo.zamariola@gmail.com>
2021-09-14 18:08:47 -0300

    [OPRT-277] Updating python script to read from file

--- exec_scripts/exec_from_file_list.py
+++ exec_scripts/exec_from_file_list.py
@@ -4,8 +4,12 @@ import json
 
 image_name = "axe-accessibility-runtime"
 
+def read_url_list(path: str) -> list[str]:
+    f = open(path, "r")
+    return f.readlines()
 
-def run_analysis_for(url: str):
+
+def run_analysis_for_url(url: str):
     cli = docker.from_env()
     return cli.containers.run(
         image=image_name,
@@ -13,7 +17,19 @@ def run_analysis_for(url: str):
         command=url)
 
 
+def run_analysis_for_list(urls: list):
+    
+    result = {}
+    
+    for url in urls:
+        output = run_analysis_for_url(url)
+        output_json = json.loads(output)
+        result[url] = output_json
+    
+    return result
+
 file_name = sys.argv[len(sys.argv) - 1]
-output = run_analysis_for(file_name)
-data = json.loads(output)
+urls = read_url_list(file_name)
+data = run_analysis_for_list(urls)
 print(data)
+
