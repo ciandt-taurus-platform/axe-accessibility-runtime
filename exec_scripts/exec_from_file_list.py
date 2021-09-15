7e074393dd1aaa3d7a7b9bd9e665bb834fc833c0
Leonardo Zamariola <leonardo.zamariola@gmail.com>
2021-09-15 10:05:09 -0300

    [OPRT-277] Updating python script to receive parameters

--- exec_scripts/exec_from_file_list.py
+++ exec_scripts/exec_from_file_list.py
@@ -1,15 +1,15 @@
 import docker
 import sys
 import json
+import getopt
 
-image_name = "axe-accessibility-runtime"
 
 def read_url_list(path: str) -> list[str]:
     f = open(path, "r")
     return f.readlines()
 
 
-def run_analysis_for_url(url: str):
+def run_analysis_for_url(url: str, image_name: str):
     cli = docker.from_env()
     return cli.containers.run(
         image=image_name,
@@ -17,19 +17,46 @@ def run_analysis_for_url(url: str):
         command=url)
 
 
-def run_analysis_for_list(urls: list):
+def run_analysis_for_list(urls: list, image_name: str):
     
     result = {}
     
     for url in urls:
-        output = run_analysis_for_url(url)
+        output = run_analysis_for_url(url, image_name)
         output_json = json.loads(output)
         result[url] = output_json
     
     return result
 
-file_name = sys.argv[len(sys.argv) - 1]
-urls = read_url_list(file_name)
-data = run_analysis_for_list(urls)
-print(data)
+def main(argv):
+   
+    usage_msg = "Usage: python exec_from_file_list.py -f <inputfile> -i <scanner_docker_image_name>"
 
+    input_file = ''
+    image_name = "axe-accessibility-runtime"
+    try:
+        opts, args = getopt.getopt(argv,"hf:i:",["file=","image="])
+    except getopt.GetoptError:
+        print(usage_msg)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(usage_msg)
+            sys.exit()
+        elif opt in ("-f", "--file"):
+            input_file = arg
+        elif opt in ("-i", "--image"):
+            image_name = arg
+    
+    if input_file == "":
+        print(usage_msg)
+        sys.exit()
+    
+    print ("Input file: ", input_file)
+    print ("Scanner image: ", image_name)
+    urls = read_url_list(input_file)
+    data = run_analysis_for_list(urls, image_name)
+    print(json.dumps(data))
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
